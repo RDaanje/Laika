94906620b85077eda9bf6744a481ca9795fa4639
RDaanje <rdaanje@gmail.com>
2019-08-05 18:52:42 +0200

    05/08/2019 merged version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './Components/register/register.component';
-
 import { ModifyAccountComponent } from './Components/modify-account/modify-account.component';
 import { HomeComponent } from './Components/home/home.component';
 import { AccountComponent } from './Components/account/account.component';
